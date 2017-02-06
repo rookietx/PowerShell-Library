197d35e779f547d17f1c42659eafbfb9afe458af
Martyn Keigher <martyn@martynkeigher.com>
2017-02-05 19:57:35 -0600

    Update LT_AgentReset.ps1

--- LT_AgentReset.ps1
+++ LT_AgentReset.ps1
@@ -2,15 +2,12 @@ kill -processname LTSVC -Force -ErrorAction SilentlyContinue
 kill -processname LTSvcMon -Force -ErrorAction SilentlyContinue
 kill -processname LTTray -Force -ErrorAction SilentlyContinue
 
-Stop-Service -Name 'LTService' -Force -ErrorAction SilentlyContinue
-Script-Sleep -s 3
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ClientID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'LocationID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'MAC' -ErrorAction SilentlyContinue
 
 Set-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ServerPassword' -Value 'MV2UJ1vycEyaYwlvQP078A=='
-
 
 Start-Sleep -s 5
 Start-Service -Name 'LTService'
