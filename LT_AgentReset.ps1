4d929134a1ef4b0c08180282ee8e9872c888aae6
Martyn Keigher <martyn@martynkeigher.com>
2017-02-05 18:10:36 -0600

    Update LT_AgentReset.ps1

--- LT_AgentReset.ps1
+++ LT_AgentReset.ps1
@@ -1,12 +1,14 @@
-##Kill Agent
-kill -processname LTSVC, LTSVCMon, LTTray -Force -ErrorAction SilentlyContinue
+kill -processname LTSVC -Force -ErrorAction SilentlyContinue
+kill -processname LTSvcMon -Force -ErrorAction SilentlyContinue
+kill -processname LTTray -Force -ErrorAction SilentlyContinue
 
-##Reset Agent
+Stop-Service -Name 'LTService' -Force -ErrorAction SilentlyContinue
+Stop-Service -Name 'LTSVCMon' -Force -ErrorAction SilentlyContinue
+
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ClientID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'LocationID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'MAC' -ErrorAction SilentlyContinue
 
-##Restart Agent
-Restart-Service -Name 'LTService'
-Restart-Service -Name 'LTSVCMon'
+Start-Service -Name 'LTService'
+Start-Service -Name 'LTSVCMon'
