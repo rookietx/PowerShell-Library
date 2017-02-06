9951ab5632e3620a6190972423649a6fddf10a9c
Martyn Keigher <martyn@martynkeigher.com>
2017-02-05 19:40:10 -0600

    Update LT_AgentReset.ps1

--- LT_AgentReset.ps1
+++ LT_AgentReset.ps1
@@ -3,14 +3,14 @@ kill -processname LTSvcMon -Force -ErrorAction SilentlyContinue
 kill -processname LTTray -Force -ErrorAction SilentlyContinue
 
 Stop-Service -Name 'LTService' -Force -ErrorAction SilentlyContinue
-Start-Sleep 1
-Stop-Service -Name 'LTSVCMon' -Force -ErrorAction SilentlyContinue
-
+Script-Sleep -s 3
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ClientID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'LocationID' -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'MAC' -ErrorAction SilentlyContinue
 
-Start-Service -Name 'LTService'
+Set-ItemProperty -Path 'HKLM:\Software\LabTech\Service' -Name 'ServerPassword' -Value 'MV2UJ1vycEyaYwlvQP078A=='
+
+
 Start-Sleep -s 5
-Start-Service -Name 'LTSVCMon'
+Start-Service -Name 'LTService'
