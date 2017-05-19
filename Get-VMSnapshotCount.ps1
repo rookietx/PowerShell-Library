7767e71e8a928158523c856ad43a977ca3bf4b96
Martyn Keigher <martyn@martynkeigher.com>
2017-05-19 18:05:18 -0500

    Update Get-VMSnapshotCount.ps1

--- Get-VMSnapshotCount.ps1
+++ Get-VMSnapshotCount.ps1
@@ -7,10 +7,7 @@ function Get-VMSnapshotCount {
         $vm
         ) 
         
-Install-PackageProvider -Name Nuget -MinimumVersion 2.8.5.201 -Scope AllUsers -Force | Out-Null
-Install-Module -Name VMware.PowerCLI –Scope AllUsers | Out-Null
-
-Start-Sleep -s 10
+Install-Module -Name VMware.PowerCLI -Scope AllUsers -ea SilentlyContinue | Out-Null
 
 Import-Module VMware.PowerCLI | Out-Null
 Connect-VIServer $esxhost -User $user -Password $pass -WarningAction SilentlyContinue -Force | Out-Null
