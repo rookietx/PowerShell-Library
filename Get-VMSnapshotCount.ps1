bd3229481934ec7d0a2bcf2c24e342c0a39c29fc
Martyn Keigher <martyn@martynkeigher.com>
2017-05-20 11:58:25 -0500

    Update Get-VMSnapshotCount.ps1

--- Get-VMSnapshotCount.ps1
+++ Get-VMSnapshotCount.ps1
@@ -9,7 +9,7 @@ function Get-VMSnapshotCount {
         
 Install-Module -Name VMware.PowerCLI -Scope AllUsers -ea SilentlyContinue | Out-Null
 
-Import-Module VMware.PowerCLI | Out-Null
+Import-Module VMware.VimAutomation.Core | Out-Null
 Connect-VIServer $esxhost -User $user -Password $pass -WarningAction SilentlyContinue -Force | Out-Null
 Get-VM * | ?{$_.Name -eq $vm} | Get-snapshot | Select-object VM | Group-Object VM | fl -Property count
 }
