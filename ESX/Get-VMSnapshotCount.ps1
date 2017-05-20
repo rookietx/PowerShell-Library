1af0e9f27c1ec37982f3f2f0dbcc6c682cc47d72
Martyn Keigher <martyn@martynkeigher.com>
2017-05-20 13:20:30 -0500

    added notes

--- ESX/Get-VMSnapshotCount.ps1
+++ ESX/Get-VMSnapshotCount.ps1
@@ -1,3 +1,41 @@
+<#PSScriptInfo
+.Version
+    1.0
+.Author
+    Martyn T. Keigher (@martynkeigher)
+.Tags
+    esx, snapshots
+.My Github URL
+    https://github.com/MartynKeigher/PowerShell-Library
+.ReleaseNotes 
+    Initial Release.
+#>
+
+
+<#
+.Description
+    Returns the number of snapshots currently live on a VM.
+.Inputs
+    System.String
+.Outputs
+    System.String
+.Parameters
+	esxhost
+	user
+	pass
+.Example
+	PS C:\> Get-Item $env:WinDir\System32\WindowsPowerShell\v1.0\powershell.exe | .\Get-VMSnapshotCount.ps1 -esxhost 192.168.10.3 -user root -pass P@ssw0rd
+.Example
+	iwr https://raw.githubusercontent.com/MartynKeigher/PowerShell-Library/master/ESX/Get-VMSnapshotCount.ps1 | iex; Get-VMSnapshotCount.ps1 -esxhost 192.168.10.3 -user root -pass P@ssw0rd
+.Notes
+    Requires the VMWare.PowerCLI to be loaded.
+.LINK
+    PowerCLI module setup guide: https://blogs.vmware.com/PowerCLI/2017/04/powercli-install-process-powershell-gallery.html
+.LINK
+	Powershell Gallery: https://www.powershellgallery.com/packages/VMware.PowerCLI/6.5.1.5377412
+.LIN
+#>
+
 function Get-VMSnapshotCount {
 
     param(
